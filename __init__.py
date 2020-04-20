54e2448643c2cb253e50b38e80778c96e201ea8f
Py-Ryan <63752837+Py-Ryan@users.noreply.github.com>
2020-04-20 18:33:02 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,12 +3,12 @@
 """Python module adding small functionality for Text -> Hexadecimal, and vice versa conversions.
 
 :copyright: (c) 2020 Py-Ryan
-:license: MIT, see LICENSE for more details.
+:license: GNU Affero General Public License v3.0, see LICENSE for more details.
 
 """
 
 __all__ = ['pysh.py']
-__version__ = '4.19.2020'
+__version__ = '4.20.2020'
 __title__ = 'pysh'
 __license__ = "MIT"
 __author__ = "Py-Ryan"
